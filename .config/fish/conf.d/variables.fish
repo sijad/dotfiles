fb0dcb067bb127b08483e0f2adf895cc2ef6bbbe
Sajjad Hashemian <wolaws@gmail.com>
2019-04-27 11:44:02 +0430

    ditch sddm

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -1,4 +1,5 @@
 # variables
+set -gx BROWSER $HOME/bin/firefox
 set -gx EDITOR nvim
 set -gx GIT_EDITOR $EDITOR
 # set -gx NDK_ROOT $HOME/android-ndk
--- .config/fish/conf.d/wayland.fish
+++ .config/fish/conf.d/wayland.fish
@@ -2,3 +2,7 @@
 # set -gx CLUTTER_BACKEND wayland
 # set -gx QT_QPA_PLATFORM wayland-egl
 set -gx GTK_THEME "Arc-Darker"
+
+if test (tty) = "/dev/tty1"
+  exec sway
+end
