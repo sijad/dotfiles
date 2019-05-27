25e1bc426e3bd631351b381b315579ac08941e44
Sajjad Hashemian <wolaws@gmail.com>
2019-05-27 07:46:13 +0430

    use MOZ_ENABLE_WAYLAND

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -3,3 +3,4 @@ set -gx BROWSER $HOME/bin/firefox
 set -gx EDITOR nvim
 set -gx GIT_EDITOR $EDITOR
 # set -gx NDK_ROOT $HOME/android-ndk
+set -gx XDG_CONFIG_HOME $HOME/.config
--- .config/fish/conf.d/wayland.fish
+++ .config/fish/conf.d/wayland.fish
@@ -2,6 +2,7 @@
 # set -gx CLUTTER_BACKEND wayland
 # set -gx QT_QPA_PLATFORM wayland-egl
 set -gx GTK_THEME "Arc-Darker"
+set -gx MOZ_ENABLE_WAYLAND 1
 
 if test (tty) = "/dev/tty1"
   exec sway
