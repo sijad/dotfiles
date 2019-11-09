e9df4913e3ecd83d38107d933499321cf5d9637e
Sajjad Hashemian <wolaws@gmail.com>
2019-11-09 11:04:16 +0330

    add android-sdk tools/bin to path

--- .config/fish/conf.d/android-sdk.fish
+++ .config/fish/conf.d/android-sdk.fish
@@ -1,3 +1,3 @@
 set -gx ANDROID_HOME $HOME/android-sdk
 
-set -gx PATH $PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools
+set -gx PATH $PATH $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools
