49de24c35b888a88c0dd4661ca8ca840a4b9d476
Sajjad Hashemian <hi@hashemian.me>
2020-09-19 18:52:41 +0430

    sync

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -1,6 +1,5 @@
 # variables
-set -gx BROWSER $HOME/bin/firefox
+set -gx BROWSER firefox
+set -gx TERMINAL st
 set -gx EDITOR nvim
 set -gx GIT_EDITOR $EDITOR
-# set -gx NDK_ROOT $HOME/android-ndk
-set -gx XDG_CONFIG_HOME $HOME/.config
