01bdfa046b6082163a29ac9567e15a23bdfc8859
Sajjad Hashemian <hi@hashemian.me>
2021-06-22 23:12:32 +0430

    cleanup sway

---
+++ .config/sway/scripts/lock.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+exec swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color 666666 --key-hl-color aaaaaa --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --fade-in 0.2 $@
+
--- .config/sway/scripts/open_terminal.sh
+++ .config/sway/scripts/open_terminal.sh
@@ -2,7 +2,7 @@
 
 # source https://www.reddit.com/r/swaywm/comments/bdseiq/modreturn_to_open_new_termite_window_in_the_same/el0h6js?utm_source=share&utm_medium=web2x
 
-terminal=${1:-termite}
+terminal=${1:-alacritty}
 pid=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.type=="con") | select(.focused==true).pid')
 pname=$(ps -p ${pid} -o comm= | sed 's/-$//')
 
@@ -14,4 +14,4 @@ then
   wpath=$(readlink /proc/${ppid}/cwd || print $HOME)
 fi
 
-swaymsg -t command exec $terminal -- --directory $wpath
+swaymsg -t command exec $terminal -- --working-directory $wpath
