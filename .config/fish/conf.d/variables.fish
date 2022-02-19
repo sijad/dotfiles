552b26476f65442bd23afa83373e8094be386d39
Sajjad Hashemian <hi@hashemian.me>
2022-02-19 12:33:55 +0330

    add firefox env

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -1,5 +1,7 @@
 # variables
-set -gx BROWSER "env MOZ_ENABLE_WAYLAND=1 MOZ_DBUS_REMOTE=1 firefox"
+set -gx MOZ_ENABLE_WAYLAND 1
+set -gx MOZ_DBUS_REMOTE 1
+set -gx BROWSER firefox
 set -gx EDITOR nvim
 set -gx GIT_EDITOR $EDITOR
 set fish_greeting
