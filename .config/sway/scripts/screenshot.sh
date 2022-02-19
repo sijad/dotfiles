d275a3cd5b6cca638538c329fd1cec1e6cf4267f
Sajjad Hashemian <hi@hashemian.me>
2022-02-19 12:32:48 +0330

    change screenshots name

--- .config/sway/scripts/screenshot.sh
+++ .config/sway/scripts/screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-SC_FILE=$HOME/Pictures/$(date '+%Y-%m-%d-%T')-screenshot.png
+SC_FILE=$HOME/Pictures/$(date '+%Y-%m-%d-%H-%M-%S')-screenshot.png
 slurp | grim -g - $SC_FILE
 wl-copy -t text/uri-list file://$SC_FILE
